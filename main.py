743996b3dc5f74183dc45e26f0c0e6f09c886748
quarioo <kirillzubrilin.zzz@gmail.com>
2023-02-06 17:29:12 +0300

    problem №3 solution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -22,6 +22,25 @@ def promotion():
                       'Присоединяйся!']
 
     return '</br>'.join(promotion_list)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f'''<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Привет, Марс!</title>
+                </head>
+                <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/riana.jpg')}" 
+                    alt="здесь должна была быть картинка, но не нашлась">
+                    <figcaption>подпись под картинкой</figcaption>
+                </body>
+                </html>'''
 
 
 if __name__ == '__main__':
